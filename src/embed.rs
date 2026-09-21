f95d82c38bc1af544cd601968ebe14eb73be94aa
Casper <CasualX@users.noreply.github.com>
2026-09-21 22:00:08 +0200

    feat: support nested, tuple, and union fields in span_of!

--- src/embed.rs
+++ src/embed.rs
@@ -1,16 +1,29 @@
+
 /// Macro to include binary data at compile time.
+///
+/// The embedded bytes are interpreted as the native representation of `$ty`.
+/// In particular, multi-byte integer and floating-point values use the target platform's endianness.
+///
+/// The file size must be an exact multiple of `size_of::<$ty>()`.
 ///
 /// ```
 /// dataview::embed!(pub DATA: [u16] = "embed.rs");
 /// ```
-#[doc(hidden)]
 #[macro_export]
 macro_rules! embed {
 	($vis:vis $name:ident: [$ty:ty] = $path:expr) => {
-		$vis static $name: [$ty; include_bytes!($path).len() / ::core::mem::size_of::<$ty>()] = {
+		$vis static $name: [$ty; {
+			const SIZE: usize = ::core::mem::size_of::<$ty>();
+			const LEN: usize = ::core::include_bytes!($path).len();
+
+			assert!(SIZE != 0, "cannot embed a zero-sized type");
+			assert!(LEN % SIZE == 0, "embedded file size is not a multiple of the element size");
+
+			LEN / SIZE
+		}] = {
 			fn __assert_pod<T: $crate::Pod>() {}
 			let _ = __assert_pod::<$ty>;
-			unsafe { ::core::mem::transmute(*include_bytes!($path)) }
+			unsafe { ::core::mem::transmute(*::core::include_bytes!($path)) }
 		};
 	};
 }
--- src/offset_of.rs
+++ src/offset_of.rs
@@ -56,10 +56,6 @@ macro_rules! __offset_of2 {
 /// ```
 ///
 /// The syntax is `$ty.$field`.
-///
-/// No support for tuples, tuple structs or unions.
-///
-/// No support for projecting through multiple fields.
 #[macro_export]
 macro_rules! span_of {
 	($($tt:tt)*) => {
@@ -70,28 +66,19 @@ macro_rules! span_of {
 #[doc(hidden)]
 #[macro_export]
 macro_rules! __span_of {
-	([$($ty:tt)*] . $($field:ident)?) => {const {
-		type Ty = $($ty)*;
-		// Assert that field exists on the type
-		// This prevents auto-Deref from causing UB
-		let Ty { $($field)?: _, .. };
-		// Use MaybeUninit as the subject of the field offset
-		let uninit = ::core::mem::MaybeUninit::<Ty>::uninit();
-		let uninit_ptr = uninit.as_ptr();
-		// We've asserted that the field exists on the type
-		// No Deref coercion or dereferencing a reference
-		// Hope that's enough to keep the code safe
-		#[allow(unused_unsafe)]
-		unsafe {
-			let field_ptr = ::core::ptr::addr_of!((*uninit_ptr).$($field)?);
-			let start = (field_ptr as *const u8).offset_from(uninit_ptr as *const u8) as usize;
-			let end = (field_ptr.offset(1) as *const u8).offset_from(uninit_ptr as *const u8) as usize;
-			start..end
+	([$($ty:tt)*] . $($field:tt)+) => {const {
+		const fn field_size<T, F>(_: fn(*const T) -> *const F) -> usize {
+			::core::mem::size_of::<F>()
 		}
+
+		let start = ::core::mem::offset_of!($($ty)*, $($field)+);
+		let size = field_size::<$($ty)*, _>(|ptr| unsafe {
+			&raw const (*ptr).$($field)+
+		});
+
+		start..start + size
 	}};
-	([$($ty:tt)*] . $($field:tt)?) => {
-		compile_error!("offset of tuple field not supported")
-	};
+
 	([$($ty:tt)*] $tt:tt $($tail:tt)*) => {
 		$crate::__span_of!([$($ty)* $tt] $($tail)*)
 	};
@@ -101,12 +88,82 @@ macro_rules! __span_of {
 }
 
 #[test]
-fn nested_fields() {
+fn tests() {
 	#[repr(C)]
-	struct Foo<T> { byte: u8, value: T }
+	struct Inner {
+		byte: u8,
+		value: u32,
+	}
 
-	assert_eq!(offset_of!(Foo<i32>.value), 4);
-	assert_eq!(span_of!(Foo<i32>.value), 4..8);
+	#[repr(C)]
+	struct TupleStruct(u16, u32);
+
+	#[repr(C)]
+	union Union {
+		byte: u8,
+		value: u32,
+	}
+
+	#[repr(C)]
+	struct Everything {
+		byte: u8,
+		inner: Inner,
+		tuple_struct: TupleStruct,
+		tuple: (u16, u32),
+		union: Union,
+	}
+
+	// Struct fields.
+	assert_eq!(offset_of!(Everything.byte), 0);
+	assert_eq!(span_of!(Everything.byte), 0..1);
+
+	assert_eq!(offset_of!(Everything.inner), 4);
+	assert_eq!(span_of!(Everything.inner), 4..12);
+
+	assert_eq!(offset_of!(Everything.inner.byte), 4);
+	assert_eq!(span_of!(Everything.inner.byte), 4..5);
+
+	assert_eq!(offset_of!(Everything.inner.value), 8);
+	assert_eq!(span_of!(Everything.inner.value), 8..12);
+
+	// Tuple struct syntax.
+	let _ = offset_of!(Everything.tuple_struct.0);
+	let _ = span_of!(Everything.tuple_struct.0);
+	let _ = offset_of!(Everything.tuple_struct.1);
+	let _ = span_of!(Everything.tuple_struct.1);
+
+	let _ = offset_of!(TupleStruct.0);
+	let _ = span_of!(TupleStruct.0);
+	let _ = offset_of!(TupleStruct.1);
+	let _ = span_of!(TupleStruct.1);
+
+	// Tuple syntax.
+	let _ = offset_of!(Everything.tuple.0);
+	let _ = span_of!(Everything.tuple.0);
+	let _ = offset_of!(Everything.tuple.1);
+	let _ = span_of!(Everything.tuple.1);
+
+	type Tuple = (u16, u32);
+	let _ = offset_of!(Tuple.0);
+	let _ = span_of!(Tuple.0);
+	let _ = offset_of!(Tuple.1);
+	let _ = span_of!(Tuple.1);
+
+	// Union fields.
+	assert_eq!(offset_of!(Everything.union), 28);
+	assert_eq!(span_of!(Everything.union), 28..32);
+
+	assert_eq!(offset_of!(Everything.union.byte), 28);
+	assert_eq!(span_of!(Everything.union.byte), 28..29);
+
+	assert_eq!(offset_of!(Everything.union.value), 28);
+	assert_eq!(span_of!(Everything.union.value), 28..32);
+
+	assert_eq!(offset_of!(Union.byte), 0);
+	assert_eq!(span_of!(Union.byte), 0..1);
+
+	assert_eq!(offset_of!(Union.value), 0);
+	assert_eq!(span_of!(Union.value), 0..4);
 }
 
 #[cfg(doc)]
